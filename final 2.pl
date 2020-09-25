0955c056fe768f94438cbd8f71ab14cfe45421c6
Ingeniero-1 <55857569+Ingeniero-1@users.noreply.github.com>
2020-09-25 14:30:56 -0300

    Add files via upload
    Version inicial

---
+++ final 2.pl
@@ -0,0 +1,74 @@
+%% final de septiembre 2020, yugioh
+
+monstruo(magoOscuro,2500,2100,1998,hechizero).
+monstruo(dragonBlanco,3000,2500,1998,dragon).
+monstruo(ojosRojos,2400,2000,1998,dragon).
+monstruo(magoDelTiempo,500,400,2003,hechizero).
+monstruo(grafa,2700,1800,2011,demonio).
+monstruo(jinzo,2400,1500,2004,maquina).
+monstruo(neos,2500,2000,2006,guerrero).
+
+magia(monstruoRenacido,invocar,1).
+magia(agujeroNegro,destruir,3).
+
+trampa(cilindrosMagicos,danio,3).
+trampa(juicioSolemne,destruir,3).
+
+protagonista(yugi,primera(magoOscuro, 10)).
+protagonista(kaiba,primera(dragonBlanco, 5)).
+protagonista(jaden,segunda(neos, 10)).
+
+
+estaLimitada(Carta):-
+    magia(Carta,_,1).
+
+estaLimitada(Carta):-
+    trampa(Carta,_,1).
+
+monstruoUtil(Monstruo):-
+    monstruo(Monstruo,_,_,_,_),
+    tieneMuchoAtaque(Monstruo).
+
+tieneMuchoAtaque(Monstruo):-
+    monstruo(Monstruo,Ataque,_,_,_),
+    Ataque > 2450.
+
+monstruoConocido(Monstruo):-
+    monstruo(Monstruo,_,_,X,_),
+    monstruoUtil(Monstruo),
+    X < 2000. 
+
+afectadoPor(jinzo, Carta):-
+    not(trampa(Carta,_,_)).
+
+afectadoPor(Monstruo, Carta):-
+    monstruo(Monstruo,_,_,_,hechizero),
+    not(magia(Carta,_,_)).
+
+puedeDestruir(Carta, Monstruo):-
+    afectadoPor(Monstruo, Carta),
+    trampa(Carta,destruir,_).
+
+puedeDestruir(Carta, Monstruo):-
+    afectadoPor(Monstruo, Carta),
+    magia(Carta,destruir,_).
+
+puedeDestruir(Atacante, Defensor):-
+    monstruo(Atacante, X, _, _, _),
+    monstruo(Defensor, Y, _, _, _),
+    X >= Y. 
+
+personajeConocido(Personaje):-
+    protagonista(Personaje, primera(_,_)).
+
+asDelProta(Nombre, As):-
+    protagonista(Nombre, primera(As, _)).
+
+asDelProta(Nombre, As):-
+    protagonista(Nombre, segunda(As, _)).
+
+mejorPersonaje(Personaje):-
+    personajeConocido(Personaje),
+    asDelProta(Personaje, As);
+	magia(Carta, _, _),
+    forall(monstruo(As, _, _, _, _), not(puedeDestruir(Carta, As))). 
